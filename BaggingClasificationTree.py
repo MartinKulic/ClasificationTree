23c81484366e028f971969ac17026fdaba83672e
ja <kulich2003@gmail.com>
2025-12-30 20:25:37 +0100

    baggin multithreading

--- BaggingClasificationTree.py
+++ BaggingClasificationTree.py
@@ -1,8 +1,13 @@
+from _thread import lock
 from concurrent.futures import ThreadPoolExecutor
+from threading import Lock
+from typing import Any
 
 import numpy as np
 import pandas as pd
 from joblib.memory import NotMemorizedFunc
+from numpy import dtype, ndarray
+from pandas import DataFrame
 
 from ClasificationTree import ClasificationTree
 
@@ -16,26 +21,38 @@ class BagingClasificationTree:
 
     def fit(self, X:pd.DataFrame, y: np.ndarray):
         self.classes = np.sort(np.unique(y))
-        for tree in self.tree:
-            this_X = [None] * X.shape[0]
-            this_y = [None] * X.shape[0]
-            # vyber s opakovanim z X,y
-            for i in range(X.shape[0]):
-                rand_idx = np.random.randint(X.shape[0])
-                this_X[i] = X.iloc[rand_idx, :]
-                this_y[i] = y[rand_idx]
 
-            this_X_df = pd.DataFrame(this_X, columns=X.columns)
-            tree.fit(this_X_df, np.array(this_y))
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            for tree in self.tree:
+                executor.submit(self.fit_task, tree, X, y) #self.fit_task(tree, X, y)
 
+    def fit_task(self, tree: ClasificationTree, X: DataFrame, y: ndarray[tuple[Any, ...], dtype[Any]]):
+        this_X = [None] * X.shape[0]
+        this_y = [None] * X.shape[0]
+        # vyber s opakovanim z X,y
+        for i in range(X.shape[0]):
+            rand_idx = np.random.randint(X.shape[0])
+            this_X[i] = X.iloc[rand_idx, :]
+            this_y[i] = y[rand_idx]
+
+        this_X_df = pd.DataFrame(this_X, columns=X.columns)
+        tree.fit(this_X_df, np.array(this_y))
 
     def predict(self, X):
         predictions = []
-        for tree in self.tree:
-            predictions.append(tree.predict(X))
+        mutex = Lock()
+
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            for tree in self.tree:
+                executor.submit(self.method_name, tree, X, predictions, mutex)#self.method_name(tree, X, predictions, mutex)
 
         values, counts = np.unique(predictions, return_counts=True)
         return values[np.argmax(counts)]
+
+    def method_name(self, tree: ClasificationTree, X, predictions: list[Any], mutex: lock):
+        prediction = tree.predict(X)
+        with mutex:
+            predictions.append(prediction)
 
     @staticmethod
     def prepareX (x: pd.DataFrame) -> pd.DataFrame:
